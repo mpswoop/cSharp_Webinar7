1d87efa7e1266e1e0d3fc18ba7a40d86e2b9571c
MP <mp@swoop.pro>
2023-08-01 09:59:49 +0300

    task52 halfdone - counting rows, not columns

---
+++ homework/task52_avarage_from_each_column/Program.cs
@@ -0,0 +1,91 @@
+﻿// create table
+// replace first row by last row
+
+int[,] tabel = new int[4, 2];
+
+void FillTabel()
+{
+    for (int i = 0; i < tabel.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabel.GetLength(1); j++)
+        {
+            tabel[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintTabel()
+{
+    for (int i = 0; i < tabel.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabel.GetLength(1); j++)
+        {
+            Console.Write(tabel[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] sum = new double[tabel.GetLength(0)];
+
+
+void FindSum()
+{
+
+    for (int i = 0; i < tabel.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabel.GetLength(1); j++)
+        {
+            sum[i] += tabel[i, j];
+            // Console.WriteLine($"_________{i} {j}");
+            // Console.Write(sum[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+double[] average = new double[tabel.GetLength(1)];
+
+void FindAverage()
+{
+
+    for (int i = 0; i < tabel.GetLength(1); i++)
+    {
+
+        average[i] = sum[i] / (tabel.GetLength(0));
+    }
+
+}
+
+void PrintSum()
+{
+    Console.WriteLine();
+
+    for (int i = 0; i < sum.GetLength(0); i++)
+    {
+        Console.Write(sum[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+void PrintAverage()
+{
+    Console.WriteLine();
+
+    for (int i = 0; i < average.GetLength(0); i++)
+    {
+        Console.Write(average[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+FillTabel();
+PrintTabel();
+Console.WriteLine("");
+FindSum();
+FindAverage();
+PrintSum();
+PrintAverage();
+
